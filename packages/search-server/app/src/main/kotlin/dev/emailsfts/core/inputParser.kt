473409d7f28e34eb3c547786cb57ea18a8e9c254
vsol <45532845+blipk@users.noreply.github.com>
2026-04-05 23:49:47 +1000

    Add fuzzy matching via class extension

--- packages/search-server/app/src/main/kotlin/dev/emailsfts/core/inputParser.kt
+++ packages/search-server/app/src/main/kotlin/dev/emailsfts/core/inputParser.kt
@@ -5,6 +5,7 @@
 package dev.emailsfts.core
 
 import org.apache.lucene.search.Query
+import org.apache.lucene.search.FuzzyQuery
 import org.apache.lucene.queryparser.classic.MultiFieldQueryParser
 
 
@@ -19,11 +20,27 @@ class SearchInputParser(
         "body" to 1.0f,
         "sender" to 1.5f
     )
-    private val luceneQueryParser = MultiFieldQueryParser(
+
+    // Create an object instance that extends the Lucene parser
+    // and overrides newTermQuery to turn term queries into terms with fuzzy expansion
+    private val luceneQueryParser = object : MultiFieldQueryParser(
         arrayOf("subject", "body", "sender", "senderName"),
         appConfig.luceneAnalyzer,
         boosts
-    )
+    ) {
+        override fun newTermQuery(term: org.apache.lucene.index.Term, boost: Float) =
+            if (appConfig.fuzzyMatchDistance > 0)
+                FuzzyQuery(term, appConfig.fuzzyMatchDistance)
+            else
+                super.newTermQuery(term, boost)
+
+        // These could also be useful to change in the future
+        override fun getWildcardQuery(field: String, termStr: String) =
+            super.getWildcardQuery(field, termStr)
+
+        override fun getFuzzyQuery(field: String, termStr: String, minSimilarity: Float) =
+            super.getFuzzyQuery(field, termStr, minSimilarity)
+    }
 
     fun parse(rawQuery: String): Query {
 
@@ -33,16 +50,12 @@ class SearchInputParser(
             .replace(Regex("to:"), "receiver:")
             .replace(Regex("has:attachments"), "hasAttachments:1")
 
-        // handle fuzzy expansion for misspellings options:
-        //  - regex to append lucene ~ fuzzy operator to appropriate terms
-        //  - override appropriate methods on MultiFieldQueryParser
-
         // lucene already handles boolean operators (AND, OR, NOT)
-        // as well as quoated phrases and other fields that already match the schema
+        // as well as quoted phrases and other fields that already match the schema
         // the frontend should provide a guide on available fields and lucene query syntax
 
         // this will throw an error on a bad query,
-        // need to add try/catch and bubble areas up to user layer
+        // need to add try/catch and bubble errors up to user layer
         return luceneQueryParser.parse(intermediateQuery)
 
     }
